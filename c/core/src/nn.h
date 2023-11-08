3b0332a95386c07ef18e3fcd4255971c58078c27
takayama3 <imtakabaka@gmail.com>
2023-11-08 21:28:24 +0300

    commit

--- c/core/src/nn.h
+++
@@ -1,4 +0,0 @@
-double FullConnected_calculate_error(FullConnected* layer, DataSet* data_set);
-void FullConnected_train(FullConnected* layer, DataSet* data_set, int epochs);
-int FullConnected_train_alpha(FullConnected* layer, DataSet* data_set, double alpha);
-void FullConnected_check(FullConnected* layer, DataSet* data_set);
